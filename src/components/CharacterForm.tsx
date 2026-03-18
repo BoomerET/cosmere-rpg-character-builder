53344ca35c5aaa8fdef65c3abac478e0ad0438be
David Berkompas <david.berkompas@gmail.com>
2026-03-18 01:39:03 -0500

    Yes, found another one
    and there's more

--- src/components/CharacterForm.tsx
+++ src/components/CharacterForm.tsx
@@ -262,7 +262,7 @@ export function CharacterForm({
               level: values.meta.level ?? 1,
               tier: getTierFromLevel(values.meta.level ?? 1),
               gender: values.meta.gender ?? "",
-              age: values.meta.age ?? 0,
+              age: values.meta.age ?? "",
               height: values.meta.height ?? "",
               weight: values.meta.weight ?? "",
               size: values.meta.size ?? "",
@@ -447,7 +447,7 @@ export function CharacterForm({
 
               <label className="field field-small">
                 <span>Age</span>
-                <input type="number" min="0" {...numericRegister("meta.age")} />
+                <input {...form.register("meta.gender")} />
               </label>
 
               <label className="field">
